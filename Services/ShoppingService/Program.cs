03949aae69d1223cb2bd7b6c5e3a9c110c723b89
Aaron Ballagan <aaronbee2010@live.co.uk>
2024-08-22 22:04:33 +0100

    Configure Swagger/SwaggerUI within Program.cs (forgot these APIs before)

--- Services/ShoppingService/Program.cs
+++ Services/ShoppingService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Shopping Service API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +22,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Shopping Service API v1");
+        options.DocumentTitle = "Shopping Service API v1";
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
